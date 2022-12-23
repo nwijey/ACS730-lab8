de3505094eedc6631935c075343e029f37d7f633
“naveen” <nwijeyasekaran@myseneca.ca>
2022-12-23 16:43:28 +0000

    all code

---
+++ terraform/modules/global_vars/output.tf
@@ -0,0 +1,17 @@
+output "default_tags" {
+  value = {
+
+    Environment = "Test"
+    Owner       = "Naveen"
+    Project     = "Ansible"
+
+  }
+}
+
+output "prefix" {
+  value ="week8"
+}
+
+output "env" {
+  value ="dev"
+}
